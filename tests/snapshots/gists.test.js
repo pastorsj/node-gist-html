56fa819800c41e32ebbb736ba8f7f1bb506751ff
pastorsj <pastorsj@rose-hulman.edu>
2017-08-04 14:09:15 -0400

    Added basic tests (they do nothing yet, just used for the ci), added babelrc file

---
+++ tests/snapshots/gists.test.js
@@ -0,0 +1,10 @@
+import chai, { expect } from 'chai';
+import sinonChai from 'sinon-chai';
+
+chai.use(sinonChai);
+
+describe('Gist Snapshot Testing', () => {
+    it('should be implemented...', () => {
+        expect(true).to.be.true;
+    });
+});
---
+++ tests/snapshots/github.test.js
@@ -0,0 +1,10 @@
+import chai, { expect } from 'chai';
+import sinonChai from 'sinon-chai';
+
+chai.use(sinonChai);
+
+describe('Github Snapshot Testing', () => {
+    it('should be implemented...', () => {
+        expect(true).to.be.true;
+    });
+});
