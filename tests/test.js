36e4c5f220c5d05f25976db52aa817ca2d7eebd4
pastorsj <pastorsj@rose-hulman.edu>
2017-08-14 11:52:20 -0400

    Add types file

--- tests/test.js
+++ tests/test.js
@@ -3,14 +3,12 @@
 const gistify = require('../lib/node-gist-html.min');
 const fs = require('fs');
 
-// html.gistify('gist', {
-//     id: 'dcb242de864e5d2b1c552783a7a00794'
-// }).then((result) => {
-//     fs.writeFileSync('index.html', result.html);
-// });
+// gistify('gist', 'dcb242de864e5d2b1c552783a7a00794')
+//     .then((result) => {
+//         fs.writeFileSync('index.html', result.html);
+//     });
 
-gistify('github', {
-    url: 'https://github.com/staltz/react-native-node/blob/master/android/build.gradle'
-}).then((result) => {
-    fs.writeFileSync('index.html', result.html);
-});
+gistify('github', 'https://github.com/staltz/react-native-node/blob/master/android/build.gradle')
+    .then((result) => {
+        fs.writeFileSync('index.html', result.html);
+    });
