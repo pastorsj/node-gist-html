126e55b348fbd5850e4c1ad77f12c77aaf545428
pastorsj <pastorsj@rose-hulman.edu>
2017-08-17 11:10:59 -0400

    Committed typings file

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for node-gist-html 1.0
+// Project: https://github.com/pastorsj/node-gist-html
+// Definitions by: Sam Pastoriza <https://github.com/pastorsj>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/**
+ * Comverts the gist or github link to html
+ * @param type Either gist or github
+ * @param link The link to the github file or gist link
+ */
+export default function gistify(type: string, link: string): Promise<string>;
