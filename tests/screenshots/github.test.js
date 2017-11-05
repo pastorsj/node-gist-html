6c448c0f61132c9a646e07e3d411f827d46fe095
pastorsj <pastorsj@rose-hulman.edu>
2017-11-05 16:55:17 -0500

    Added tests for removed footer and line numbers options

--- tests/screenshots/github.test.js
+++ tests/screenshots/github.test.js
@@ -7,8 +7,9 @@ const HTML_FILE = path.join(__dirname, 'test.html');
 const OUTPUT_DIRECTORY = path.join(__dirname, 'output');
 const REFERENCE_DIRECTORY = path.join(__dirname, 'reference');
 const DIFF_DIRECTORY = path.join(__dirname, 'diff');
+const url = 'https://github.com/pastorsj/pandora-clone/blob/master/AudioStreaming/src/main/java/pandora/clone/AudioStreamingApplication.java';
 
-describe.only('Github Snapshot Testing', () => {
+describe('Github Snapshot Testing', () => {
     afterEach((done) => {
         try {
             if (fs.existsSync(HTML_FILE)) {
@@ -19,9 +20,9 @@ describe.only('Github Snapshot Testing', () => {
             done(e);
         }
     });
-    describe('Standard Conversion', () => {
+    describe('Github: Standard Conversion', () => {
+        const file = 'github_standard.png';
         before((done) => {
-            const url = 'https://github.com/pastorsj/pandora-clone/blob/master/AudioStreaming/src/main/java/pandora/clone/AudioStreamingApplication.java';
             retrieveScreenshot(done, url, HTML_FILE);
         });
         it('should return html that is formatted correctly', (done) => {
@@ -34,9 +35,105 @@ describe.only('Github Snapshot Testing', () => {
             driver.get(`file:///${HTML_FILE}`)
                 .then(() => driver.takeScreenshot())
                 .then((image) => {
-                    const newImageLocation = path.join(OUTPUT_DIRECTORY, 'github_standard.png');
-                    const originalImageLocation = path.join(REFERENCE_DIRECTORY, 'github_standard.png');
-                    const diffImageLocation = path.join(DIFF_DIRECTORY, 'github_standard.png');
+                    const newImageLocation = path.join(OUTPUT_DIRECTORY, file);
+                    const originalImageLocation = path.join(REFERENCE_DIRECTORY, file);
+                    const diffImageLocation = path.join(DIFF_DIRECTORY, file);
+                    fs.writeFileSync(newImageLocation, image, 'base64');
+                    driver.quit();
+                    return compareImages(newImageLocation, originalImageLocation, diffImageLocation);
+                })
+                .then((comparisonResult) => {
+                    if (!comparisonResult) {
+                        done(new Error('A visual difference was detected and a diff file was generated'));
+                    } else {
+                        done();
+                    }
+                })
+                .catch(err => done(err));
+        });
+    });
+    describe('Github: Removed Line Numbers', () => {
+        const file = 'github_remove_line_numbers.png';
+        before((done) => {
+            retrieveScreenshot(done, url, HTML_FILE, { removeLineNumbers: true });
+        });
+        it('should return html that is formatted correctly', (done) => {
+            const driver = new webdriver.Builder()
+                .withCapabilities(webdriver.Capabilities.chrome())
+                .build();
+
+            driver.manage().window().maximize();
+
+            driver.get(`file:///${HTML_FILE}`)
+                .then(() => driver.takeScreenshot())
+                .then((image) => {
+                    const newImageLocation = path.join(OUTPUT_DIRECTORY, file);
+                    const originalImageLocation = path.join(REFERENCE_DIRECTORY, file);
+                    const diffImageLocation = path.join(DIFF_DIRECTORY, file);
+                    fs.writeFileSync(newImageLocation, image, 'base64');
+                    driver.quit();
+                    return compareImages(newImageLocation, originalImageLocation, diffImageLocation);
+                })
+                .then((comparisonResult) => {
+                    if (!comparisonResult) {
+                        done(new Error('A visual difference was detected and a diff file was generated'));
+                    } else {
+                        done();
+                    }
+                })
+                .catch(err => done(err));
+        });
+    });
+    describe('Github: Removed Footer', () => {
+        const file = 'github_removed_footer.png';
+        before((done) => {
+            retrieveScreenshot(done, url, HTML_FILE, { removeFooter: true });
+        });
+        it('should return html that is formatted correctly', (done) => {
+            const driver = new webdriver.Builder()
+                .withCapabilities(webdriver.Capabilities.chrome())
+                .build();
+
+            driver.manage().window().maximize();
+
+            driver.get(`file:///${HTML_FILE}`)
+                .then(() => driver.takeScreenshot())
+                .then((image) => {
+                    const newImageLocation = path.join(OUTPUT_DIRECTORY, file);
+                    const originalImageLocation = path.join(REFERENCE_DIRECTORY, file);
+                    const diffImageLocation = path.join(DIFF_DIRECTORY, file);
+                    fs.writeFileSync(newImageLocation, image, 'base64');
+                    driver.quit();
+                    return compareImages(newImageLocation, originalImageLocation, diffImageLocation);
+                })
+                .then((comparisonResult) => {
+                    if (!comparisonResult) {
+                        done(new Error('A visual difference was detected and a diff file was generated'));
+                    } else {
+                        done();
+                    }
+                })
+                .catch(err => done(err));
+        });
+    });
+    describe('Github: Removed Line Numbers and Footer', () => {
+        const file = 'github_removed_line_numbers_and_footer.png';
+        before((done) => {
+            retrieveScreenshot(done, url, HTML_FILE, { removeFooter: true, removeLineNumbers: true });
+        });
+        it('should return html that is formatted correctly', (done) => {
+            const driver = new webdriver.Builder()
+                .withCapabilities(webdriver.Capabilities.chrome())
+                .build();
+
+            driver.manage().window().maximize();
+
+            driver.get(`file:///${HTML_FILE}`)
+                .then(() => driver.takeScreenshot())
+                .then((image) => {
+                    const newImageLocation = path.join(OUTPUT_DIRECTORY, file);
+                    const originalImageLocation = path.join(REFERENCE_DIRECTORY, file);
+                    const diffImageLocation = path.join(DIFF_DIRECTORY, file);
                     fs.writeFileSync(newImageLocation, image, 'base64');
                     driver.quit();
                     return compareImages(newImageLocation, originalImageLocation, diffImageLocation);
